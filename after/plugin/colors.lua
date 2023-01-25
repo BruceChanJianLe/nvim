15f16e42cbbe86c57f0436b9d4a842ff62943525
brucechanjianle <jianle001@e.ntu.edu.sg>
2023-01-25 21:28:10 +0800

    add colorpencil function

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,13 +1,14 @@
--- function ColorMyPencils(color) 
--- 	color = color or "rose-pine"
--- 	vim.cmd.colorscheme(color)
--- 
--- 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
--- 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
--- 
--- end
--- 
--- ColorMyPencils()
-
 -- All builtin colorschemes can be accessed with |:colorscheme|.
-vim.cmd('colorscheme base16-gruvbox-dark-hard')
+function ColorMyPencils(color)
+	color = color or "base16-gruvbox-dark-hard"
+	vim.cmd.colorscheme(color)
+
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+
+end
+
+vim.keymap.set("n", "<leader>c", ":lua ColorMyPencils()<CR>")
+
+ColorMyPencils()
+-- vim.cmd('colorscheme base16-gruvbox-dark-hard')
