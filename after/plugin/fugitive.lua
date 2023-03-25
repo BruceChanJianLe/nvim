cae4a9b4af0d6ecd96c9c0ebbe833dd0c019eeb5
brucechanjianle <jianle001@e.ntu.edu.sg>
2023-03-25 21:33:27 +0800

    add more git remaps
    pull and fetch

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -1,2 +1,4 @@
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git);
 vim.keymap.set("n", "<leader>gp", "<cmd>Git push<CR>");
+vim.keymap.set("n", "<leader>gu", "<cmd>Git pull<CR>");
+vim.keymap.set("n", "<leader>gf", "<cmd>Git fetch<CR>");
