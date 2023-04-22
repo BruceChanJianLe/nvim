9bedb0451c5efc0b7410a1d72648d5210d4649c3
brucechanjianle <jianle001@e.ntu.edu.sg>
2023-04-22 16:33:42 +0800

    use neotree as explorer...yay!

--- lua/user/packer.lua
+++ lua/user/packer.lua
@@ -11,18 +11,18 @@ return require('packer').startup(function(use)
     'nvim-telescope/telescope.nvim', tag = '0.1.1',
     -- or                            , branch = '0.1.x',
     requires = { 'nvim-lua/plenary.nvim',
-    {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' },
-    "nvim-telescope/telescope-rg.nvim",
+      { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' },
+      "nvim-telescope/telescope-rg.nvim",
+    }
   }
-}
 
   -- use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
 
 
-  use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+  use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
   use('nvim-treesitter/playground')
   use('theprimeagen/harpoon')
-  use{'mbbill/undotree'}
+  use { 'mbbill/undotree' }
 
   -- Text object manipulation
   use('tpope/vim-surround')
@@ -31,51 +31,51 @@ return require('packer').startup(function(use)
   -- color setup
   use('RRethy/nvim-base16')
   -- use{"lukas-reineke/indent-blankline.nvim"}
-  use{"Mofiqul/vscode.nvim"}
+  use { "Mofiqul/vscode.nvim" }
 
   use {
     'VonHeikemen/lsp-zero.nvim',
     requires = {
       -- LSP Support
-      {'neovim/nvim-lspconfig'},
-      {'williamboman/mason.nvim'},
-      {'williamboman/mason-lspconfig.nvim'},
+      { 'neovim/nvim-lspconfig' },
+      { 'williamboman/mason.nvim' },
+      { 'williamboman/mason-lspconfig.nvim' },
 
       -- Autocompletion
-      {'hrsh7th/nvim-cmp'},
-      {'hrsh7th/cmp-buffer'},         -- Using words buffers
-      {'hrsh7th/cmp-path'},           -- Using file(name)s
-      {'hrsh7th/cmp-nvim-lua'},       -- For when writing lua in nvim
-      {'hrsh7th/cmp-nvim-lsp'},       -- LSP support and moving snippets
-      {'saadparwaiz1/cmp_luasnip'},   -- For lua snippets
+      { 'hrsh7th/nvim-cmp' },
+      { 'hrsh7th/cmp-buffer' },     -- Using words buffers
+      { 'hrsh7th/cmp-path' },       -- Using file(name)s
+      { 'hrsh7th/cmp-nvim-lua' },   -- For when writing lua in nvim
+      { 'hrsh7th/cmp-nvim-lsp' },   -- LSP support and moving snippets
+      { 'saadparwaiz1/cmp_luasnip' }, -- For lua snippets
 
       -- Snippets
-      {'L3MON4D3/LuaSnip'},
+      { 'L3MON4D3/LuaSnip' },
       -- Snippet Collection (Optional)
-      {'rafamadriz/friendly-snippets'},
+      { 'rafamadriz/friendly-snippets' },
     }
   }
 
   -- Autocompletion for pairs
-  use {"windwp/nvim-autopairs"}
+  use { "windwp/nvim-autopairs" }
   -- Tabout from pairs
-  use {'abecodes/tabout.nvim'}
+  use { 'abecodes/tabout.nvim' }
 
   -- Git plugins
   use('tpope/vim-fugitive')
-  use{'lewis6991/gitsigns.nvim'}
+  use { 'lewis6991/gitsigns.nvim' }
 
   -- Commenting
-  use{'numToStr/Comment.nvim'}
+  use { 'numToStr/Comment.nvim' }
 
   -- Statusline
   use {
     'nvim-lualine/lualine.nvim',
-    requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+    requires = { 'nvim-tree/nvim-web-devicons', opt = true }
   }
 
   -- Terminal
-  use {"akinsho/toggleterm.nvim", tag = '*'}
+  use { "akinsho/toggleterm.nvim", tag = '*' }
 
   -- Buffer tabs
   use {
@@ -87,4 +87,15 @@ return require('packer').startup(function(use)
     }
   }
 
+  -- File explorer
+  use {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v2.x",
+    requires = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons",
+      "MunifTanjim/nui.nvim",
+    }
+  }
+  use ("nvim-tree/nvim-web-devicons") -- For icons!
 end)
