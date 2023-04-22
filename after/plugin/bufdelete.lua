ecdd1e89368dc916dfaf28a867464bc4f40e8cc4
brucechanjianle <jianle001@e.ntu.edu.sg>
2023-04-22 20:57:31 +0800

    shortcut for closing buffer

--- after/plugin/bufdelete.lua
+++ after/plugin/bufdelete.lua
@@ -2,3 +2,6 @@ local status_ok, bufdelete = pcall(require, "bufdelete")
 if not status_ok then
   return
 end
+
+vim.keymap.set("n", "<leader><leader>q", "<cmd>Bdelete<CR>")
+vim.keymap.set("n", "<leader>c", "<cmd>Bdelete<CR>")
