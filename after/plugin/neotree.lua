a1ffd903f8cbdfc5d55ef8ecd9cbc7c8d1753500
brucechanjianle <jianle001@e.ntu.edu.sg>
2023-04-23 16:23:07 +0800

    update formatting and use neotree for cwd

--- after/plugin/neotree.lua
+++ after/plugin/neotree.lua
@@ -24,14 +24,14 @@ neotree.setup({
     },
     indent = {
       indent_size = 2,
-      padding = 1,   -- extra padding on left hand side
+      padding = 1, -- extra padding on left hand side
       -- indent guides
       with_markers = true,
       indent_marker = "│",
       last_indent_marker = "└",
       highlight = "NeoTreeIndentMarker",
       -- expander config, needed for nesting files
-      with_expanders = nil,   -- if nil and file nesting is enabled, will enable expanders
+      with_expanders = nil, -- if nil and file nesting is enabled, will enable expanders
       expander_collapsed = "",
       expander_expanded = "",
       expander_highlight = "NeoTreeExpander",
@@ -57,8 +57,8 @@ neotree.setup({
     git_status = {
       symbols = {
         -- Change type
-        added     = "",    -- or "✚", but this is redundant info if you use git_status_colors on the name
-        modified  = "",    -- or "", but this is redundant info if you use git_status_colors on the name
+        added     = "",  -- or "✚", but this is redundant info if you use git_status_colors on the name
+        modified  = "",  -- or "", but this is redundant info if you use git_status_colors on the name
         deleted   = "✖", -- this can only be used in the git_status source
         renamed   = "", -- this can only be used in the git_status source
         -- Status type
@@ -84,7 +84,7 @@ neotree.setup({
     mappings = {
       ["<space>"] = {
         "toggle_node",
-        nowait = false,   -- disable `nowait` if you have existing combos starting with this char that you want to use
+        nowait = false, -- disable `nowait` if you have existing combos starting with this char that you want to use
       },
       ["<2-LeftMouse>"] = "open",
       ["l"] = "open",
@@ -112,23 +112,23 @@ neotree.setup({
         -- this command supports BASH style brace expansion ("x{a,b,c}" -> xa,xb,xc). see `:h neo-tree-file-actions` for details
         -- some commands may take optional config options, see `:h neo-tree-mappings` for details
         config = {
-          show_path = "none"   -- "none", "relative", "absolute"
+          show_path = "none" -- "none", "relative", "absolute"
         }
       },
-      ["A"] = "add_directory",   -- also accepts the optional config.show_path option like "add". this also supports BASH style brace expansion.
+      ["A"] = "add_directory", -- also accepts the optional config.show_path option like "add". this also supports BASH style brace expansion.
       ["d"] = "delete",
       ["r"] = "rename",
       ["y"] = "copy_to_clipboard",
       ["x"] = "cut_to_clipboard",
       ["p"] = "paste_from_clipboard",
-      ["c"] = "copy",   -- takes text input for destination, also accepts the optional config.show_path option like "add":
+      ["c"] = "copy", -- takes text input for destination, also accepts the optional config.show_path option like "add":
       -- ["c"] = {
       --  "copy",
       --  config = {
       --    show_path = "none" -- "none", "relative", "absolute"
       --  }
       --}
-      ["m"] = "move",       -- takes text input for destination, also accepts the optional config.show_path option like "add".
+      ["m"] = "move", -- takes text input for destination, also accepts the optional config.show_path option like "add".
       ["q"] = "close_window",
       ["R"] = "refresh",
       ["?"] = "show_help",
@@ -139,21 +139,21 @@ neotree.setup({
   nesting_rules = {},
   filesystem = {
     filtered_items = {
-      visible = false,       -- when true, they will just be displayed differently than normal items
+      visible = false, -- when true, they will just be displayed differently than normal items
       hide_dotfiles = true,
       hide_gitignored = true,
-      hide_hidden = true,       -- only works on Windows for hidden files/directories
+      hide_hidden = true, -- only works on Windows for hidden files/directories
       hide_by_name = {
         --"node_modules"
       },
-      hide_by_pattern = {       -- uses glob style patterns
+      hide_by_pattern = { -- uses glob style patterns
         --"*.meta",
         --"*/src/*/tsconfig.json",
       },
-      always_show = {     -- remains visible even if other settings would normally hide it
+      always_show = { -- remains visible even if other settings would normally hide it
         --".gitignored",
       },
-      never_show = {   -- remains hidden even if visible is toggled to true, this overrides always_show
+      never_show = { -- remains hidden even if visible is toggled to true, this overrides always_show
         --".DS_Store",
         --"thumbs.db"
       },
@@ -161,10 +161,10 @@ neotree.setup({
         --".null-ls_*",
       },
     },
-    follow_current_file = false,          -- This will find and focus the file in the active buffer every
+    follow_current_file = false,            -- This will find and focus the file in the active buffer every
     -- time the current file is changed while the tree is open.
-    group_empty_dirs = false,             -- when true, empty folders will be grouped together
-    hijack_netrw_behavior = "open_default", -- netrw disabled, opening a directory opens neo-tree
+    group_empty_dirs = false,               -- when true, empty folders will be grouped together
+    hijack_netrw_behavior = "open_current", -- netrw disabled, opening a directory opens neo-tree
     -- in whatever position is specified in window.position
     -- "open_current",  -- netrw disabled, opening a directory opens within the
     -- window like netrw would, regardless of window.position
@@ -186,7 +186,7 @@ neotree.setup({
         ["]g"] = "next_git_modified",
       },
       fuzzy_finder_mappings = {
-                              -- define keymaps for filter popup window in fuzzy_finder_mode
+        -- define keymaps for filter popup window in fuzzy_finder_mode
         ["<down>"] = "move_cursor_down",
         ["<C-n>"] = "move_cursor_down",
         ["<up>"] = "move_cursor_up",
@@ -196,9 +196,9 @@ neotree.setup({
     commands = {} -- Add a custom command or override a global one using the same function name
   },
   buffers = {
-    follow_current_file = true,       -- This will find and focus the file in the active buffer every
+    follow_current_file = true, -- This will find and focus the file in the active buffer every
     -- time the current file is changed while the tree is open.
-    group_empty_dirs = true,          -- when true, empty folders will be grouped together
+    group_empty_dirs = true,    -- when true, empty folders will be grouped together
     show_unloaded = true,
     window = {
       mappings = {
