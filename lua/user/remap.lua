6a9502203e98f5856b3c9d70db83719d3665aaad
brucechanjianle <jianle001@e.ntu.edu.sg>
2023-06-21 22:51:07 +0800

    follow current file in neotree

--- lua/user/remap.lua
+++ lua/user/remap.lua
@@ -2,6 +2,8 @@ vim.g.mapleader = " "
 -- project view
 vim.keymap.set("n", "<leader>pv", "<cmd>Neotree focus reveal<CR>", { desc = '[P]roject [V]iew' })
 vim.keymap.set("n", "<C-b>", "<cmd>Neotree toggle reveal<CR>", { desc = 'Follow vscode convention' })
+vim.keymap.set("n", "<leader>pv", "<cmd>Neotree focus<CR>", { desc = '[P]roject [V]iew' })
+vim.keymap.set("n", "<C-b>", "<cmd>Neotree toggle<CR>", { desc = 'Follow vscode convention' })
 
 -- move entire line in visual mode
 vim.keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv", { desc = 'move entire line down in visual mode' })
