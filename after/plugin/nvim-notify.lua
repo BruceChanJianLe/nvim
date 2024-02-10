d5ac0daa5541dd75be2f297d6920524b54b25ce1
Brina Shong <brinashong@gmail.com>
2024-02-10 19:26:50 +0800

    clear notification with lua function and keymaps

--- after/plugin/nvim-notify.lua
+++ after/plugin/nvim-notify.lua
@@ -4,3 +4,10 @@ if not status_ok then
 end
 
 vim.notify = notify
+
+-- Clear notification
+vim.keymap.set("n", "<leader><leader>c", ":lua require('notify').dismiss()<cr>",  { desc = '[C]lear Notifications' })
+vim.keymap.set("v", "<leader><leader>c", ":<BS><BS><BS><BS><BS>lua require('notify').dismiss()<cr>gv",  { desc = '[C]lear Notifications' })
+function ClearNotifications()
+  require('notify').dismiss()
+end
