36f801331ddfcdcaa05bec574aa12ebbf2dad5cb
Chan Jian Le <chanjl@i2r.a-star.edu.sg>
2024-08-16 10:49:49 +0800

    wip: enable oil plugin
    It requires to be initialize throught config function.

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -2,20 +2,25 @@
 return {
   {
     'stevearc/oil.nvim',
-    opts = {
-
-      default_file_explorer = false,
-      -- delete_to_trash = true,
-      skip_confirm_for_simple_edits = true,
-      view_options = {
-        show_hidden = true,
-        natural_order = true,
-      },
-      win_options = {
-        wrap = true,
-      }
-    },
+    opts = {},
     config = function()
+      local status_ok, oil = pcall(require, "oil")
+      if not status_ok then
+        return
+      end
+
+      oil.setup({
+        default_file_explorer = false,
+        -- delete_to_trash = true,
+        skip_confirm_for_simple_edits = true,
+        view_options = {
+          show_hidden = true,
+          natural_order = true,
+        },
+        win_options = {
+          wrap = true,
+        }
+      })
       -- Open parent directory in current window
       vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 
