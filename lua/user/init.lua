92313907898ffe55233037c8e972aeac2a323c4e
Chan Jian Le <chanjl@i2r.a-star.edu.sg>
2024-08-18 13:26:25 +0800

    removing manual colorscheme setup seems to work

--- lua/user/init.lua
+++ lua/user/init.lua
@@ -4,7 +4,7 @@ require("user.lazy")
 
 -- setting colorscheme last
 -- an issue for another day
-vim.o.background = 'dark'
-vim.cmd.colorscheme('vscode')
--- vim.cmd('highlight link TreesitterContext PmenuSbar')
-vim.api.nvim_set_hl(0, 'TreesitterContext', { link = 'PmenuSbar' })
+-- vim.o.background = 'dark'
+-- vim.cmd.colorscheme('vscode')
+-- -- vim.cmd('highlight link TreesitterContext PmenuSbar')
+-- vim.api.nvim_set_hl(0, 'TreesitterContext', { link = 'PmenuSbar' })
