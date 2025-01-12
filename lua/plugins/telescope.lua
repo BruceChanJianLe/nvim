51900a3f75e960f1fa8972718e7e44f59d61596d
Chan Jian Le <chanjl@i2r.a-star.edu.sg>
2025-01-12 22:40:00 +0800

    wip: use search file to look into project files

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -58,7 +58,7 @@ return {
       -- vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
       vim.keymap.set('n', "<leader>fg", "<cmd>Telescope live_grep<cr>", { desc = '[S]earch by [G]rep' })
 
-      -- vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+      vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
       -- vim.keymap.set('n', '<C-p>', builtin.find_files, {})
       vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = '[S]earch [F]iles' })
 
