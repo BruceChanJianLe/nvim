8825cdec3cdf0e59d933cfede98998336c08c6c4
Chan Jian Le <chanjl@i2r.a-star.edu.sg>
2025-02-14 12:34:34 +0800

    wip: sync up common key input

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -191,8 +191,8 @@ return {
               -- ["D"] = "fuzzy_sorter_directory",
               ["f"] = "filter_on_submit",
               ["<c-x>"] = "clear_filter",
-              ["[g"] = "prev_git_modified",
-              ["]g"] = "next_git_modified",
+              ["[c"] = "prev_git_modified",
+              ["]c"] = "next_git_modified",
             },
             fuzzy_finder_mappings = {
               -- define keymaps for filter popup window in fuzzy_finder_mode
@@ -225,7 +225,7 @@ return {
               ["gu"] = "git_unstage_file",
               ["ga"] = "git_add_file",
               ["gr"] = "git_revert_file",
-              ["gc"] = "git_commit",
+              ["cc"] = "git_commit",
               ["gp"] = "git_push",
               ["gg"] = "git_commit_and_push",
             }
