98795966fd2170ae8c8d17b091a8c3e5d649e9cf
Chan Jian Le <chanjl@i2r.a-star.edu.sg>
2025-04-03 15:48:53 +0800

    wip: update install deps
    Install clangd!

--- install_neovim.bash
+++ install_neovim.bash
@@ -82,7 +82,7 @@ if [[ $DEPS == "true" ]]
 then
   echo "Installing depedencies for building neovim..."
   sudo apt-get install ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip curl doxygen ripgrep -y -qq
-  sudo apt-get install python3-venv luarocks imagemagick -y -qq
+  sudo apt-get install python3-venv luarocks imagemagick clangd -y -qq
 fi
 
 # Checks if neovim directory already exists
