2ce7f20f48229c84bc386485d31bca643a899f79
Chan Jian Le <chanjl@i2r.a-star.edu.sg>
2025-06-02 22:00:08 +0800

    wip: display only trailing spaces when typing
    Let's try the Jason Turner way :D

--- lua/plugins/indent_blankline.lua
+++ lua/plugins/indent_blankline.lua
@@ -1,14 +1,22 @@
+-- Plan to move to set.lua
 vim.opt.list = true
-vim.opt.listchars:append "space:·"
-vim.opt.listchars:append "eol:¶"
-vim.opt.listchars:append "trail:·"
-vim.opt.listchars:append "tab:→ "
+vim.opt.listchars = {
+  trail = "·",
+  -- space = "·",
+  tab = "→ ",
+  eol= "¶"
+}
+
+vim.keymap.set("n", "<leader>ti", function()
+  vim.opt.list = not vim.opt.list:get()
+end, { desc = "[T]oggle [I]nvisible Characters" })
+
 return {
-  {
-    "lukas-reineke/indent-blankline.nvim",
-    main = "ibl",
-    ---@module "ibl"
-    ---@type ibl.config
-    opts = {},
-  }
+  -- {
+  --   "lukas-reineke/indent-blankline.nvim",
+  --   main = "ibl",
+  --   ---@module "ibl"
+  --   ---@type ibl.config
+  --   opts = {},
+  -- }
 }
