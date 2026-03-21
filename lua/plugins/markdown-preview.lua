5c6aae70b1a203253fa34b53cfe4667492fae0fc
Chan Jian Le <jianle001@e.ntu.edu.sg>
2026-03-21 12:20:08 +0800

    chore: some notes for user and my future self

--- lua/plugins/markdown-preview.lua
+++ lua/plugins/markdown-preview.lua
@@ -8,12 +8,15 @@ return {
   config = function()
     vim.keymap.set("n", "<leader>v", "<cmd>MarkdownPreviewToggle<CR>");
 
+    -- Access the markdown with the printed link
+    -- ssh -L 3333:localhost:3333 user@remoteserver
     vim.g.mkdp_port = "3333"
     vim.g.mkdp_echo_preview_url = 1
     vim.g.mkdp_auto_start = 0
     vim.g.mkdp_auto_close = 1        -- closes preview when you leave the buffer
     vim.g.mkdp_refresh_slow = 0      -- live refresh as you type
     vim.g.mkdp_browser = ""          -- don't try to open a browser on the server
+
     -- vim.g.mkdp_browserfunc = 'OpenMarkdownPreview'
     -- -- Ref: https://github.com/iamcco/markdown-preview.nvim/issues/262
     -- vim.cmd(
