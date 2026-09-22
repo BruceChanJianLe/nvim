695b00b3934b8cab2d1f59231c9302574e904491
Chan Jian Le <jianle001@e.ntu.edu.sg>
2026-09-22 21:49:19 +0800

    chore: document the new member for toggle code action

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -9,12 +9,12 @@ return {
 
       -- Document existing key chains
       require('which-key').add {
-        { '<leader>c', group = '[C]ode' },
         { '<leader>d', group = '[D]ocument' },
         { '<leader>r', group = '[R]ename' },
         { '<leader>s', group = '[S]earch/[S]et' },
         -- [T]oggle group: tn numbers, ti invisibles, tw wrap, tp paste,
-        -- tt terminal, tc ts-context, th inlay hints, ts C++ src/header
+        -- tt terminal, tc ts-context, th inlay hints, ts C++ src/header,
+        -- ta LSP code actions
         { '<leader>t', group = '[T]oggle' },
         { '<leader>p', group = '[P]roject' },
         -- { '<leader>w', group = '[W]orkspace' },
